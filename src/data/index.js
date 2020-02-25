6b57f2710b55e83b279024e025e84e91fef1870d
Kyle Richardson <kyle.richardson1@gmail.com>
2020-02-25 08:04:52 -0600

    buttons working for search

--- src/data/index.js
+++ src/data/index.js
@@ -4,7 +4,7 @@ export const projects = [
     url: "https://kyle-todo.netlify.com",
     github: "https://github.com/kyle-richardson/React-Todo/",
     image: "./images/to-do.png",
-    tools: ["css3", "html5", "react"],
+    tools: ["css3", "html5", "js", "react"],
     notes: ""
   },
   {
@@ -12,7 +12,7 @@ export const projects = [
     url: "https://kyle-lambda-calculator.netlify.com",
     github: "https://github.com/kyle-richardson/lambda-calculator",
     image: "./images/calculator.png",
-    tools: ["css3", "html5", "react"],
+    tools: ["css3", "html5", "js", "react"],
     notes: ""
   },
   {
@@ -20,7 +20,7 @@ export const projects = [
     url: "https://kyle-github-network.netlify.com",
     github: "https://github.com/kyle-richardson/React-Github-User-Card",
     image: "./images/github.png",
-    tools: ["css3", "html5", "react"],
+    tools: ["css3", "html5", "js", "react"],
     notes: ""
   },
 
@@ -29,14 +29,14 @@ export const projects = [
     url: "https://kyle-nasa-potd.netlify.com",
     github: "https://github.com/kyle-richardson/nasa-photo-of-the-day",
     image: "./images/nasa.jpg",
-    tools: ["css3", "html5", "react"],
+    tools: ["css3", "html5", "js", "react"],
     notes: ""
   },
   {
     name: "Weightlifting Journal",
     url: "https://weightlifting-journal.netlify.com/",
     image: "./images/weightlifting.png",
-    tools: ["css3", "html5", "react", "node-js"],
+    tools: ["css3", "html5", "react", "js", "node-js"],
     libraries: ["Material UI", "Redux"],
     notes: "Team project"
   }
@@ -46,6 +46,6 @@ export const resume = {
   name: "Digital Resume",
   url: "https://www.kylerichardson.tech",
   image: "",
-  tools: ["css3, html5"],
+  tools: ["css3, html5", "less"],
   notes: ""
 };
