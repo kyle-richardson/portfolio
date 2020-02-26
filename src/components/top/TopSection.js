f4ed3d25635bf3c1dec11dd6e77d30caae11fe8d
Kyle Richardson <kyle.richardson1@gmail.com>
2020-02-25 21:04:33 -0600

    added carousel component and button filter comp

---
+++ src/components/top/TopSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+import CarouselSection from "./CarouselSection";
+import ButtonSection from "./ButtonSection";
+
+const TopSection = props => {
+  return (
+    <header id="App-header">
+      <h1>Kyle Richardson - Portfolio</h1>
+      <h3>Full-stack developer</h3>
+      {/* <CarouselSection /> */}
+      <ButtonSection {...props} />
+    </header>
+  );
+};
+
+export default TopSection;
--- src/data/index.js
+++ src/data/index.js
@@ -4,7 +4,7 @@ export const projects = [
     url: "https://www.kylerichardson.tech",
     github: "https://github.com/kyle-richardson/kyle-richardson.github.io",
     image: `${process.env.PUBLIC_URL}/assets/images/resume.png`,
-    tools: ["css3", "html5", "less"],
+    tools: ["html5", "css3", "less"],
     notes: ""
   },
   {
@@ -12,7 +12,7 @@ export const projects = [
     url: "https://kyle-todo.netlify.com",
     github: "https://github.com/kyle-richardson/React-Todo/",
     image: `${process.env.PUBLIC_URL}/assets/images/to-do.png`,
-    tools: ["css3", "html5", "js", "react"],
+    tools: ["html5", "css3", "js", "react"],
     notes: ""
   },
   {
@@ -20,7 +20,7 @@ export const projects = [
     url: "https://kyle-lambda-calculator.netlify.com",
     github: "https://github.com/kyle-richardson/lambda-calculator",
     image: `${process.env.PUBLIC_URL}/assets/images/calculator.png`,
-    tools: ["css3", "html5", "js", "react"],
+    tools: ["html5", "css3", "js", "react"],
     notes: ""
   },
   {
@@ -28,7 +28,7 @@ export const projects = [
     url: "https://kyle-github-network.netlify.com",
     github: "https://github.com/kyle-richardson/React-Github-User-Card",
     image: `${process.env.PUBLIC_URL}/assets/images/github.png`,
-    tools: ["css3", "html5", "js", "react"],
+    tools: ["html5", "css3", "js", "react"],
     notes: ""
   },
 
@@ -37,14 +37,14 @@ export const projects = [
     url: "https://kyle-nasa-potd.netlify.com",
     github: "https://github.com/kyle-richardson/nasa-photo-of-the-day",
     image: `${process.env.PUBLIC_URL}/assets/images/nasa.jpg`,
-    tools: ["css3", "html5", "js", "react"],
+    tools: ["html5", "css3", "js", "react"],
     notes: ""
   },
   {
     name: "Weightlifting Journal",
     url: "https://weightlifting-journal.netlify.com/",
     image: `${process.env.PUBLIC_URL}/assets/images/weightlifting.png`,
-    tools: ["css3", "html5", "react", "js", "node-js"],
+    tools: ["html5", "css3", "less", "js", "react", "node-js"],
     libraries: ["Material UI", "Redux"],
     notes: "Team project"
   }
